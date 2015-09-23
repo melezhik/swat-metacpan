13e78d1e497505559fac4fbd6df89b608587a879
Alexey Melezhik <melezhik@gmail.com>
2015-09-23 12:28:51 +0000

    prepare-for-next-release

--- web/CHANGELOG.md
+++ web/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1
+- check if latest release at https://metacpan.org/feed/recent?f= is not older than 4 hours ago
+
+
 # 0.1.0
 - first release at CPAN
 
--- web/lib/swat/metacpan/web.pm
+++ web/lib/swat/metacpan/web.pm
@@ -1,5 +1,5 @@
 package swat::metacpan::web; 
-our $VERSION = '0.1.0'; 
+our $VERSION = '0.1.1'; 
 1;
 
 __END__
@@ -9,7 +9,16 @@ __END__
 
 L<Swat|https://github.com/melezhik/swat> tests for metacpan.org
 
-This is simple test suit could be used in monitoring/deployment. A limited set of routes is tested.
+This is simple test suit could be used in monitoring/deployment.
+
+
+=head1 Checks perfomed
+
+- check if landing page is accessible
+- check if modules search is working properly
+- check if latest releases page/feed is accessible and returns not empty results 
+- check if latest release at https://metacpan.org/feed/recent?f= is not older than 4 hours ago
+
 
 =head1 INSTALL
 
@@ -28,4 +37,10 @@ OR
     swat swat::metacpan::web https://metacpan.org
 
 
+
+=head1 COPYRIGHT
+
+Copyright 2015 Alexey Melezhik.
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
